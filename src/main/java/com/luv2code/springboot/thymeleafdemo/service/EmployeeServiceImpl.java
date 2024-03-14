b802bab55bb44418b8571ed3d7161b63f517efca
Cfs003 <162965601+Cfs003@users.noreply.github.com>
2024-03-14 14:51:51 +0300

    Update EmployeeServiceImpl.java

--- src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -35,7 +35,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 		}
 		else {
 			// we didn't find the employee
-			throw new RuntimeException("Did not find employee id - " + theId);
+			throw new RuntimeException("Calisan bulunamadi - " + theId);
 		}
 		
 		return theEmployee;
